edc026646adcb3454cf8c66b88f7cffdbd3e73c1
shinichi <shinichitazawa.mails@gmail.com>
2021-01-13 08:21:09 +0900

    20210113

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-const PDFDocument = require('pdfkit');
-const faker = require('faker');
-const getStream = require('get-stream');
+
+const line = require('line/bot-sdk');
 var AWS = require('aws-sdk');
 var dynamo = new AWS.DynamoDB.DocumentClient();
+const line_client = new line.Client({
+    channelAccessToken: process.env.ACCESS_TOKEN
+})
 exports.lambdaHandler = async (event) => {
 
-    const doc = new PDFDocument();
+    // pdf作成
+    // const doc = new PDFDocument();
 
-    const randomName = faker.name.findName();
+    // const randomName = faker.name.findName();
 
-    doc.text(randomName, { align: 'right' });
-    doc.text(faker.address.streetAddress(), { align: 'right' });
-    doc.text(faker.address.secondaryAddress(), { align: 'right' });
-    doc.text(faker.address.zipCode() + ' ' + faker.address.city(), { align: 'right' });
-    doc.moveDown();
-    doc.text('Dear ' + randomName + ',');
-    doc.moveDown();
-    for(let i = 0; i < 3; i++) {
-        doc.text(faker.lorem.paragraph());
-        doc.moveDown();
-    }
-    doc.text(faker.name.findName(), { align: 'right' });
-    doc.end();
+    // doc.text(randomName, { align: 'right' });
+    // doc.text(faker.address.streetAddress(), { align: 'right' });
+    // doc.text(faker.address.secondaryAddress(), { align: 'right' });
+    // doc.text(faker.address.zipCode() + ' ' + faker.address.city(), { align: 'right' });
+    // doc.moveDown();
+    // doc.text('Dear ' + randomName + ',');
+    // doc.moveDown();
+    // for(let i = 0; i < 3; i++) {
+    //     doc.text(faker.lorem.paragraph());
+    //     doc.moveDown();
+    // }
+    // doc.text(faker.name.findName(), { align: 'right' });
+    // doc.end();
 
-    pdfBuffer = await getStream.buffer(doc);
-    pdfBase64 = pdfBuffer.toString('base64');
+    
+    // pdfBuffer = await getStream.buffer(doc);
+    // pdfBase64 = pdfBuffer.toString('base64');
+
+    // const response = {
+    //     statusCode: 200,
+    //     headers: {
+    //         'Content-Length': Buffer.byteLength(pdfBase64),
+    //         'Content-Type': 'application/pdf',
+    //         'Content-disposition': 'attachment;filename=test.pdf'
+    //     },
+    //     isBase64Encoded: true,
+    //     body: pdfBase64
+    // };
+    console.log('event', event);
+    event_data = JSON.parse(event.body);
+    console.log('event_data', JSON.stringfy(event_data));
+    const messageData = event_data.events && event_data.events[0]; 
 
     const response = {
-        statusCode: 200,
-        headers: {
-            'Content-Length': Buffer.byteLength(pdfBase64),
-            'Content-Type': 'application/pdf',
-            'Content-disposition': 'attachment;filename=test.pdf'
-        },
-        isBase64Encoded: true,
-        body: pdfBase64
-    };
-    return response;
+        'type': 'text',
+        'text': messageData.message.text,
+    }
+    try {
+        await line_client.replyMessage(messageData.replyToken, response)
+    } catch (e) {
+        console.log(e)
+    }
 };
